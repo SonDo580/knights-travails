96fd809bbfe019fa77ba56ac5d09bd73a9474801
Son Do <dominhson580@gmail.com>
2022-09-11 17:14:56 +0700

    Create a 2-dimensional array as gameBoard

---
+++ main.js
@@ -0,0 +1,16 @@
+function createBoard() {
+  let arr = [];
+
+  for (let i = 0; i < 8; i++) {
+    let row = [];
+    for (let j = 0; j < 8; j++) {
+      row.push("");
+    }
+    arr.push(row);
+  }
+
+  return arr;
+}
+
+let gameBoard = createBoard();
+console.log(gameBoard);
