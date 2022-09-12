b34db4d5f643fcb3909ed2d1f5254ab28578559d
Son Do <dominhson580@gmail.com>
2022-09-12 15:30:28 +0700

    Create a queue and enqueue the fist node

--- main.js
+++ main.js
@@ -3,12 +3,15 @@ const SIDE = 8;
 const rowMove = [2, 2, -2, -2, 1, 1, -1, -1];
 const colMove = [-1, 1, -1, 1, 2, -2, 2, -2];
 
-function knightMoves(src, dest) {}
-
 function NodeFactory(row, col, distance = 0) {
   return { row, col, distance };
 }
 
 function isValid(row, col) {
   return row >= 0 && row < SIDE && col >= 0 && col < SIDE;
+}
+
+function knightMoves(src, dest) {
+  const queue = [];
+  queue.push(src);
 }
