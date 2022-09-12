4cda35b98b3669c5f14c583699a212591e480829
Son Do <dominhson580@gmail.com>
2022-09-12 15:37:20 +0700

    Dequeue the front node and process it

--- main.js
+++ main.js
@@ -1,8 +1,11 @@
 const SIDE = 8;
 
+// 8 possible movements for a knight
 const rowMove = [2, 2, -2, -2, 1, 1, -1, -1];
 const colMove = [-1, 1, -1, 1, 2, -2, 2, -2];
 
+// Represent a cell as a node in a tree
+// Distance: number of moves from src
 function NodeFactory(row, col, distance = 0) {
   return { row, col, distance };
 }
@@ -11,7 +14,12 @@ function isValid(row, col) {
   return row >= 0 && row < SIDE && col >= 0 && col < SIDE;
 }
 
+// This returns the shortest path from src to dest
 function knightMoves(src, dest) {
   const queue = [];
   queue.push(src);
+
+  while (queue.length > 0) {
+    node = queue.shift();
+  }
 }
