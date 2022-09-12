2e454bfcea3711552f729426c5acd202aa33c571
Son Do <dominhson580@gmail.com>
2022-09-12 10:21:29 +0700

    Change knightMoves parameters

--- main.js
+++ main.js
@@ -1,14 +1,9 @@
 const SIDE = 8;
 
-function knightMoves(vertex1, vertex2) {
-  let startRow = vertex1[0];
-  let startCol = vertex1[1];
-  let endRow = vertex2[0];
-  let endCol = vertex2[1];
-}
-
 const rowMove = [2, 2, -2, -2, 1, 1, -1, -1];
 const colMove = [-1, 1, -1, 1, 2, -2, 2, -2];
+
+function knightMoves(src, dest) {}
 
 function NodeFactory(row, col, distance = 0) {
   return { row, col, distance };
