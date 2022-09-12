33a3798acb0bd01621a1d61bf6aae9c0ea637a4d
Son Do <dominhson580@gmail.com>
2022-09-12 10:19:08 +0700

    Remove function nextMove()

--- main.js
+++ main.js
@@ -24,55 +24,6 @@ function knightMoves(vertex1, vertex2) {
   let endCol = vertex2[1];
 }
 
-// function nextMove(row, col, endRow, endCol) {
-// let vertices = [];
-// vertices.push([row, col]);
-// if (row === endRow && col === endCol) {
-//   return vertices;
-// }
-// let paths = [];
-// if (row - 2 >= 0) {
-//   if (col - 1 >= 0) {
-//     paths.push([...vertices, ...nextMove(row - 2, col - 1, endRow, endCol)]);
-//   }
-//   if (col + 1 < SIDE) {
-//     paths.push([...vertices, ...nextMove(row - 2, col + 1, endRow, endCol)]);
-//   }
-// }
-// if (row + 2 < SIDE) {
-//   if (col - 1 >= 0) {
-//     paths.push([...vertices, ...nextMove(row + 2, col - 1, endRow, endCol)]);
-//   }
-//   if (col + 1 < SIDE) {
-//     paths.push([...vertices, ...nextMove(row + 2, col + 1, endRow, endCol)]);
-//   }
-// }
-// if (row - 1 >= 0) {
-//   if (col - 2 >= 0) {
-//     paths.push([...vertices, ...nextMove(row - 1, col - 2, endRow, endCol)]);
-//   }
-//   if (col + 2 < SIDE) {
-//     paths.push([...vertices, ...nextMove(row - 1, col + 2, endRow, endCol)]);
-//   }
-// }
-// if (row + 1 < SIDE) {
-//   if (col - 2 >= 0) {
-//     paths.push([...vertices, ...nextMove(row + 1, col - 2, endRow, endCol)]);
-//   }
-//   if (col + 2 < SIDE) {
-//     paths.push([...vertices, ...nextMove(row + 1, col + 2, endRow, endCol)]);
-//   }
-// }
-// let shortestPath = paths[0];
-// for (let path of paths) {
-//   if (path.length < shortestPath.length) {
-//     shortestPath = path;
-//   }
-// }
-// vertices = [...vertices, ...shortestPath];
-// return vertices;
-// }
-
 const rowMove = [2, 2, -2, -2, 1, 1, -1, -1];
 const colMove = [-1, 1, -1, 1, 2, -2, 2, -2];
 
