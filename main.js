5f0cc89c872debc7ae995d02193291a36feb5883
Son Do <dominhson580@gmail.com>
2022-09-12 10:17:21 +0700

    Create nodeFactory function

--- main.js
+++ main.js
@@ -76,6 +76,10 @@ function knightMoves(vertex1, vertex2) {
 const rowMove = [2, 2, -2, -2, 1, 1, -1, -1];
 const colMove = [-1, 1, -1, 1, 2, -2, 2, -2];
 
+function NodeFactory(row, col, distance = 0) {
+  return { row, col, distance };
+}
+
 function isValid(row, col) {
   return row >= 0 && row < SIDE && col >= 0 && col < SIDE;
 }
